4d365b45c6775e9e3dd66de5f93e73bdc269c1cc
Tizzerai <84933446+Tizzerai@users.noreply.github.com>
2025-09-17 20:27:31 -0600

    Starting functionality
    bare bones to pretty up once basic functionality is there

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,19 +4,20 @@ import Link from "next/link";
 
 export default function StudentInfo() {
   return (
-    <div className="p-6 max-w-md mx-auto bg-white rounded-2xl shadow-lg space-y-4">
-      <h1 className="text-2xl font-bold text-gray-900">Student Info</h1>
-      <p className="text-lg text-gray-700">
+    <div >
+      <h1 className=" font-bold text-red-600">Student Info</h1>
+      <p className=" text-red-600">
         <strong>Name:</strong> Tessa Rae Feyres
       </p>
       <div>
         <p className="text-lg font-medium text-gray-700 mb-2">GitHub Repository:</p>
         <Link
-          href="https://github.com/your-username/your-repo"
+          href="https://www.youtube.com/watch?v=rMmziyTS3YI"
+          //Placeholder link for now
           className="text-blue-600 hover:underline"
           target="_blank"
         >
-          Visit My GitHub Repo
+        GitHub Repo
         </Link>
       </div>
     </div>
