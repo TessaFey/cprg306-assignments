3be2e091af4718dc5c664b02c96472317245026c
Tizzerai <84933446+Tizzerai@users.noreply.github.com>
2025-09-17 21:24:47 -0600

    Attempting to fix upload
    Testing upload

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"CfA6GJ+QhUnHGbKu6bIPWJfRo/7FNaWHTsRc26LodAs=\"\n}"
---
+++ .next/static/chunks/app_week-2_page_ed3b780c.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_f47d3abc._.js"
+],
+    source: "dynamic"
+});
