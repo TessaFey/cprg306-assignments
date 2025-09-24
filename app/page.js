7973724faf2d6e815dc9b481daf21dfa785e6f9c
Tizzerai <84933446+Tizzerai@users.noreply.github.com>
2025-09-24 17:13:48 -0600

    main page
    week 3 now listed UNDER week 2

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div>
       <h1 className=" font-bold text-red-600">CPRG 306: Web Development 2 - Assignments</h1>
+      <div>
       <Link
           href="/week-2"
           className="text-blue-600 hover:underline"
@@ -12,13 +13,16 @@ export default function Home() {
         >
         week-2
         </Link>
+        </div>
+        <div>
         <Link
           href="/week-3"
           className="text-blue-600 hover:underline"
           target="_blank"
         >
-        week-2
+        week-3
         </Link>
+        </div>
     </div>
     
   );
